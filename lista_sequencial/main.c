64b6e2db5d6fa2266b607d825b45f214052763f0
Felipe Cecconet <wcecconet@alunos.utfpr.edu.br>
2026-03-23 13:16:51 -0300

    Inserção da main

--- lista_sequencial/main.c
+++ lista_sequencial/main.c
@@ -1,4 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "clientes.h"
 
+#define MAX_PESSOAS         50
+#define TAMANHO_MAX_NOME    100
+#define TAMANHO_MAX_RG_STR  15
+#define TAMANHO_MAX_LINHA   128
+
+int main() {
+    Pessoa minhaLista[MAX_PESSOAS];
+    
+    int totalCarregado = carregarPessoas(minhaLista, MAX_PESSOAS, "dados_rg.txt");
+
+    mostrarLista(minhaLista, totalCarregado);
+
+    return 0;
+}
